d7a1c51ac2939fea2df0e2f4368374a456e6e0f1
DrojasFrontend <daniel.rojas@leggercolombia.com>
2025-02-23 07:43:49 -0500

    guia de estilos

---
+++ template-parts/home/section-books.php
@@ -0,0 +1,25 @@
+<section>
+    <div class="container">
+      <h2 class="fs-2 d-flex align-center gap-18">Libros <span class="line d-block position-relative"></span></h2>
+
+      <div class="row">
+        <div class="col-12 col-lg-6">
+          <article class="d-flex gap-18 p-8 bg-white">
+            <div class="col-12 col-lg-5">
+              <img class="img-fluid d-flex" src="<?php echo THEME_IMG . 'libro.png'; ?>" alt="">
+            </div>
+            <div class="col-12 col-lg-7">
+              <h3 class="fs-4 fw-bold mb-18">Manual de producción de cerezas</h3>
+              <p class="mb-18">
+                Guía práctica para el cultivo y manejo de cerezas, con técnicas de producción, cuidados esenciales y estrategias para maximizar la calidad y rendimiento.
+              </p>
+              <div class="d-flex gap-8 align-center">
+                <img width="36" height="36" src="<?php echo THEME_IMG . 'avatar.png'; ?>" alt="">
+                <p class="m-0">Por Patricio Morales</p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+</section>
--- templates/template-home.php
+++ templates/template-home.php
@@ -12,16 +12,6 @@ get_header('nuevo');
 
 ?>
 
-<!-- <h1 class="fs-1">H1</h1>
-<h2 class="fs-2">H2</h2>
-<h3 class="fs-3">H3</h3>
-<h4 class="fs-4">H4</h4>
-
-<div class="p-8 bg-green">Padding 8</div>
-<div class="p-18 bg-yellow-100">Padding 18</div>
-<div class="p-50 bg-black-25">Padding 50</div>
-<div class="p-100 bg-gray">Padding 100</div> -->
-
 <!-- CONTENT -->
 <main>
   <!-- News -->
@@ -35,7 +25,6 @@ get_header('nuevo');
   <!-- News Most Recent -->
   <?php get_template_part('template-parts/home/section', 'most-recent') ?>
   <!-- News Most Recent -->
-
 
 </main>
 <!-- CONTENT -->
